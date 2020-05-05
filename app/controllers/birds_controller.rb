62383c9d89a17d231d4a0e972280ec2d6ebf28ca
SantiagoSalazarPavajeau <santiago.s.pavajeau@gmail.com>
2020-05-05 16:14:39 +0200

    Done.

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,6 +1,6 @@
 class BirdsController < ApplicationController
   def index
-    @birds = Bird.all
+    birds = Bird.all
     # render 'birds/index.html.erb'
     # render plain: "Hello #{@birds[3].name}"
     # render json: 'Remember that JSON is just object notation converted to string data, so strings also work here'
@@ -8,6 +8,6 @@ class BirdsController < ApplicationController
     # render json: ['As','well','as','arrays']
     # render json: @birds
     # create a view with json from RoR and extract that json from javascript in another html/javascript file
-
+    render json: { birds: birds, messages: ['Hello Birds', 'Goodbye Birds'] }
   end
 end
