5a33d6ecfb0f098c48f1515f6cd5b17ca0208b9c
SantiagoSalazarPavajeau <santiago.s.pavajeau@gmail.com>
2020-05-05 16:02:02 +0200

    create a view with json from RoR and extract that json from javascript in another html/javascript file

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,6 +1,13 @@
 class BirdsController < ApplicationController
   def index
     @birds = Bird.all
-    render 'birds/index.html.erb'
+    # render 'birds/index.html.erb'
+    # render plain: "Hello #{@birds[3].name}"
+    # render json: 'Remember that JSON is just object notation converted to string data, so strings also work here'
+    # render json: { message: 'Hashes of data will get converted to JSON' }
+    # render json: ['As','well','as','arrays']
+    # render json: @birds
+    # create a view with json from RoR and extract that json from javascript in another html/javascript file
+
   end
 end
